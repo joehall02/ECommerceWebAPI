1d9fef9e9143dafcef0601999b992ddd23bdfdb1
joehall02 <joehall0207@gmail.com>
2024-06-07 14:49:07 +0100

    Created schemas for both signup and login, created UserService to handle business logic outside of api endpoints, added some basic error handling and form validation

--- api/auth.py
+++ api/auth.py
@@ -1,9 +1,52 @@
 from models import User
 from flask import Flask, request
 from flask_restx import Namespace, Resource, fields
+from marshmallow import Schema, fields, ValidationError
+
+# Define the schema for the signup and login operations used to validate the request data
+class SignupSchema(Schema):
+    first_name = fields.Str(required=True)
+    last_name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    password = fields.Str(required=True)
+    phone_number = fields.Str(required=False)
+
+class LoginSchema(Schema):
+    username = fields.Str(required=True)
+    password = fields.Str(required=True)
+
+# Define the schema instances
+signup_schema = SignupSchema()
+login_schema = LoginSchema()
+
+# Services
+class UserService:
+    @staticmethod
+    def create_user(data):
+        email_exists = User.query.filter_by(email=data['email']).first()
+        phone_number_exists = User.query.filter_by(phone_number=data['phone_number']).first()
+        if email_exists or phone_number_exists:
+            raise ValidationError('User with the provided email or phone number already exists')
+
+        new_user = User(
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            password=data['password'],
+            email=data['email'],
+            phone_number=data['phone_number'],
+            role='customer'
+        )
+        new_user.save()
+        return new_user
+    
+    def login(data):
+        pass
+    
 
 auth_ns = Namespace('auth', description='Authentication operations')
 
+# Define the models for the signup and login used for api
+# documentation, actual validation is done using the schema
 signup_model = auth_ns.model('Signup', {
     'first_name': fields.String(required=True),
     'last_name': fields.String(required=True),
@@ -23,19 +66,22 @@ class Signup(Resource):
     def post(self):
         data = request.get_json()
 
-        email_exists = User.query.filter_by(email=data['email']).first()
-        if email_exists:
-            return {'message': f'An account with that email already exists'}, 400
+        # Validate the request data against the signup schema
+        try:
+            valid_data = signup_schema.load(data)
+        except ValidationError as e: # Return an error response if the request data is invalid
+            return {'message': str(e)}, 400
 
-        new_user = User(
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            password=data['password'],
-            email=data['email'],
-            phone_number=data['phone_number'],
-            role='customer'
-        )
-        new_user.save()
+        # Create a new user account
+        try:
+            UserService.create_user(valid_data)
+        except ValidationError as e:
+            return {'message': str(e)}, 400
 
         return {'message': 'Account created successfully'}, 201
-    
+
+@auth_ns.route('/login', methods=['POST'])
+class Login(Resource):
+    @auth_ns.expect(login_model)
+    def post(self):
+        data = request.get_json()
