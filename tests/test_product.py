cced9731e62686c23d0097ae5ccd38e8097582d0
joehall02 <joehall0207@gmail.com>
2024-10-15 21:03:36 +0100

    Added tests for the new image product api endpoints, created mock results so the tests don't need to interact with Google Cloud Storage

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,7 @@
 from base import BaseTestCase
+from unittest.mock import patch
+from werkzeug.datastructures import FileStorage
+import io
 
 # Tests for all product related endpoints
 class ProductTestCase(BaseTestCase):
@@ -16,6 +19,24 @@ class ProductTestCase(BaseTestCase):
 
         # Create a featured product
         self.client.post('/product/admin/featured-product/1', headers={'Authorization': 'Bearer ' + self.access_token})
+
+        # Create a mock image file
+        mock_image = FileStorage(
+            stream=io.BytesIO(b"fake image data"),
+            filename="test_image.jpg",
+            content_type="image/jpeg"
+        )
+
+        # Create a product image
+        with patch('services.product_service.upload_image_to_google_cloud_storage') as mock_upload_image_to_google_cloud_storage:
+            mock_upload_image_to_google_cloud_storage.return_value = 'bucket_name/test_image.jpg'
+        
+            self.client.post(
+                '/product/admin/product-image/1', 
+                headers={'Authorization': 'Bearer ' + self.access_token}, 
+                data={'image': mock_image}, 
+                content_type='multipart/form-data'
+            )
 
     # Product tests
     def test_create_product(self):
@@ -93,3 +114,51 @@ class ProductTestCase(BaseTestCase):
 
         # Check if the response is correct
         self.assertEqual(response.status_code, 200)
+
+    @patch('services.product_service.upload_image_to_google_cloud_storage')
+    def test_create_product_image(self, mock_upload_image_to_google_cloud_storage):
+        # Mock the upload_image_to_google_cloud_storage function
+        mock_upload_image_to_google_cloud_storage.return_value = 'bucket_name/test_image.jpg'
+        
+        # Create a mock image file
+        mock_image = FileStorage(
+            stream=io.BytesIO(b"fake image data"),
+            filename="test_image.jpg",
+            content_type="image/jpeg"
+        )
+
+        # Send a POST request to the endpoint
+        response = self.client.post(
+            '/product/admin/product-image/1', 
+            headers={'Authorization': 'Bearer ' + self.access_token}, 
+            data={'image': mock_image}, 
+            content_type='multipart/form-data'
+        )
+
+        # Check if the response is correct
+        self.assertEqual(response.status_code, 201)
+        
+        # Ensure method is called only once
+        mock_upload_image_to_google_cloud_storage.assert_called_once()
+    
+    def test_get_product_images(self):
+        # Send a GET request to the endpoint
+        response = self.client.get('/product/product-image/1', headers={'Authorization': 'Bearer ' + self.access_token})
+
+        # Check if the response is correct
+        self.assertEqual(response.status_code, 200)
+
+
+    @patch('services.product_service.remove_image_from_google_cloud_storage')
+    def test_delete_product_image(self, mock_remove_image_from_google_cloud_storage):
+        # Mock the remove_image_from_google_cloud_storage function
+        mock_remove_image_from_google_cloud_storage.return_value = None
+
+        # Send a DELETE request to the endpoint
+        response = self.client.delete('/product/admin/product-image/1', headers={'Authorization': 'Bearer ' + self.access_token})
+
+        # Check if the response is correct
+        self.assertEqual(response.status_code, 200)
+
+        # Ensure method is called only once
+        mock_remove_image_from_google_cloud_storage.assert_called_once()
