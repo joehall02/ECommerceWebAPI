5182acabc0a2e23a43872741368c61b0fb703d73
joehall02 <joehall0207@gmail.com>
2025-04-26 17:43:27 +0100

    Changes to GitHub Actions CI pipeline config.

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,13 @@
+import sys
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(['tests', '-p', 'no:warnings']) # Run the tests in the tests directory and ignore the warnings
+    # pytest.main(['tests', '-p', 'no:warnings']) # Run the tests in the tests directory and ignore the warnings
     # pytest.main(['tests']) # Run the tests in the tests directory 
+
+    # Run tests with --maxfail=1 to stop at the first failure, and --disable-warnings to suppress warnings
+    # We can also use --exitfirst to stop immediately after the first failure
+    result = pytest.main(['tests', '--maxfail=1', '--disable-warnings', '--exitfirst'])
+    
+    # If pytest fails, exit with the same status code as pytest
+    sys.exit(result)
